c63dd3d918d527425f68ed844ad264e6438ea9bc
r7kamura <r7kamura@gmail.com>
2024-05-30 12:11:09 +0900

    Seperate src files into src dir

---
+++ src/git.ts
@@ -0,0 +1,22 @@
+import { exec } from "./deps.ts";
+
+export async function createAndPushCommit({
+  userName,
+  userEmail,
+  branch,
+  message,
+  files,
+}: {
+  userName: string;
+  userEmail: string;
+  branch: string;
+  message: string;
+  files: string[];
+}) {
+  await exec.exec("git", ["config", "user.name", userName]);
+  await exec.exec("git", ["config", "user.email", userEmail]);
+  await exec.exec("git", ["switch", "--create", branch]);
+  await exec.exec("git", ["add", ...files]);
+  await exec.exec("git", ["commit", "--message", message]);
+  await exec.exec("git", ["push", "--set-upstream", "origin", branch]);
+}
---
+++ src/github.ts
@@ -0,0 +1,60 @@
+import { github } from "./deps.ts";
+import { performance } from "node:perf_hooks";
+
+// Workaround for deno compatibility issue on undici:
+// deno-lint-ignore no-explicit-any
+(globalThis as any).performance = performance;
+
+export function generateNotes({
+  githubToken,
+  owner,
+  repo,
+  tagName,
+  targetCommitish,
+}: {
+  githubToken: string;
+  owner: string;
+  repo: string;
+  tagName: string;
+  targetCommitish: string;
+}) {
+  return github.getOctokit(githubToken).request(
+    "POST /repos/{owner}/{repo}/releases/generate-notes",
+    {
+      owner,
+      repo,
+      tag_name: tagName,
+      target_commitish: targetCommitish,
+    },
+  );
+}
+
+export async function createPullRequest({
+  githubToken,
+  owner,
+  repo,
+  title,
+  body,
+  head,
+  base,
+}: {
+  githubToken: string;
+  owner: string;
+  repo: string;
+  title: string;
+  body: string;
+  head: string;
+  base: string;
+}) {
+  return await github.getOctokit(githubToken).request(
+    "POST /repos/{owner}/{repo}/pulls",
+    {
+      owner,
+      repo,
+      title,
+      body,
+      head,
+      base,
+    },
+  );
+}
