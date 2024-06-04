df833d171aefd1b00edb628c4716fd8d124f25dc
r7kamura <r7kamura@gmail.com>
2024-06-04 13:03:32 +0900

    Make command optional

---
+++ src/changeVersion.ts
@@ -0,0 +1,76 @@
+import { exec } from "./deps.ts";
+import { fs } from "./deps.ts";
+
+const version = Deno.env.get("BUMP_REQUEST_VERSION")!;
+
+if (checkCratePackage()) {
+  changeCrateVersion(version);
+} else if (checkGemPackage()) {
+  changeGemVersion(version);
+} else if (checkNpmPackage()) {
+  changeNpmVersion(version);
+} else if (checkPlainPackage()) {
+  changePlainVersion(version);
+} else {
+  throw new Error("Unsupported package type.");
+}
+
+function checkCratePackage() {
+  return fs.existsSync("Cargo.toml");
+}
+
+function checkGemPackage() {
+  return fs.expandGlobSync("lib/**/version.rb").next().value !== undefined;
+}
+
+function checkNpmPackage() {
+  return fs.existsSync("package.json");
+}
+
+function checkPlainPackage() {
+  return fs.existsSync("VERSION");
+}
+
+function changeCrateVersion(version: string) {
+  replaceContent(
+    "Cargo.toml",
+    /version = "[^"]+"/,
+    `version = "${version}"`,
+  );
+}
+
+function changeGemVersion(version: string) {
+  replaceContent(
+    fs.expandGlobSync("lib/**/version.rb").next().value.path,
+    /VERSION = "[^"]+"/,
+    `VERSION = "${version}"`,
+  );
+  exec.exec("bundle", ["install"]);
+}
+
+function changeNpmVersion(version: string) {
+  exec.exec("npm", [
+    "version",
+    "--no-git-commit-hooks",
+    "--no-git-tag-version",
+    version,
+  ]);
+}
+
+function changePlainVersion(version: string) {
+  replaceContent(
+    "VERSION",
+    /.+/,
+    version,
+  );
+}
+
+function replaceContent(
+  filePath: string,
+  pattern: RegExp,
+  replacement: string,
+) {
+  const content = Deno.readTextFileSync(filePath);
+  const updatedContent = content.replace(pattern, replacement);
+  Deno.writeTextFileSync(filePath, updatedContent);
+}
--- src/deps.ts
+++ src/deps.ts
@@ -3,3 +3,4 @@ export * as exec from "https://esm.sh/@actions/exec@1.1.1?dts";
 export * as github from "https://esm.sh/@actions/github@6.0.0?dts";
 export * as semver from "https://esm.sh/semver@7.6.2?dts";
 export * as asserts from "https://deno.land/std@0.65.0/testing/asserts.ts";
+export * as fs from "https://deno.land/std@0.224.0/fs/mod.ts";
